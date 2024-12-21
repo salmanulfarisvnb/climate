96b456ff5a4d9f50c56cb530ef1b74a2b82fb338
Salman <salmanulfarisp9946@gmail.com>
2024-12-21 14:20:24 +0530

    add api setup fineshed

---
+++ src/api/weather.ts
@@ -0,0 +1,60 @@
+import { API_CONFIG } from "./config";
+import {
+  Coordinates,
+  ForecastData,
+  GeocodingResponse,
+  WeatherData,
+} from "./types";
+
+class WeatherAPI {
+  private createUrl(endPoint: string, params: Record<string, string | number>) {
+    const searchParams = new URLSearchParams({
+      appid: API_CONFIG.API_KEY,
+      ...params,
+    });
+    return `${endPoint}?${searchParams.toString()}`;
+  }
+
+  private async fetchData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Weather API Error:${response.statusText}`);
+    }
+    return response.json();
+  }
+
+  async getCurrentWeather({ lat, lon }: Coordinates): Promise<WeatherData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units,
+    });
+
+    return this.fetchData<WeatherData>(url);
+  }
+
+  async getForecast({ lat, lon }: Coordinates): Promise<ForecastData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units,
+    });
+
+    return this.fetchData<ForecastData>(url);
+  }
+
+  async reverseGeocoding({
+    lat,
+    lon,
+  }: Coordinates): Promise<GeocodingResponse[]> {
+    const url = this.createUrl(`${API_CONFIG.GEO}/reverseGeocoding`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      limit: 1,
+    });
+
+    return this.fetchData<GeocodingResponse[]>(url);
+  }
+}
+
+export const weatherAPI = new WeatherAPI();
