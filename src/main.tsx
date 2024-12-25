ec917c98feb51362e1a33791448c767698ea6ad1
Salman <salmanulfarisp9946@gmail.com>
2024-12-25 11:15:55 +0530

    tanstack query keys added

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,16 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, //5 minutes
+      gcTime: 10 * 60 * 1000, //10 minutes
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
